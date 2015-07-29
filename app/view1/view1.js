a77ecf283f3606ce60453e48f05d87ff9f8bd55e
Jonathan Henderson <jonathan.henderson@experian.com>
2015-07-29 15:21:59 -0700

    Added graphic and chart template

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,18 +11,41 @@ angular.module('myApp.view1', ['ngRoute', 'highcharts-ng'])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
 
-        console.log("stop here");
+        //console.log("stop here");
         $scope.chartConfig = {
             options: {
                 chart: {
-                    type: 'bar'
+                    type: 'bar',
+                    backgroundColor:'rgba(255, 255, 255, 0)'
                 }
             },
+            colors: ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
+                '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'],
             series: [{
-                data: [10, 15, 12, 8, 7]
+                data: [{y:10, name:'Fred'},
+                    {y: 15, name: 'Buster'},
+                    {y: 12, name: 'Jesse'},
+                    {y: 8, name: 'Jon'},
+                    {y: 7, name: 'Melanie'}]
             }],
             title: {
-                text: 'Hello'
+                text: ''
+            },
+            yAxis: {
+                labels: {
+                    style: {
+                        color: 'white'
+                    }
+                },
+                gridLineColor: 'white'
+            },
+            xAxis: {
+                labels: {
+                    style: {
+                        color: 'white'
+                    }
+                },
+                gridLineColor: 'white'
             },
             loading: false
         }
